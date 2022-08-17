16ed7a5e0f2f8e51b8aad158ea116e7321aa4ed6
김시환 <htogether7@gmail.com>
2022-08-18 02:48:55 +0900

    EDIT : edit map
    using react-naver-maps, change map to component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,26 @@
 import React from "react";
+import { NaverMap, RenderAfterNavermapsLoaded } from "react-naver-maps";
+
 const Map = () => {
-  return <div id="map"></div>;
+  return (
+    <RenderAfterNavermapsLoaded
+      ncpClientId={process.env.REACT_APP_MAP_CLIENT_ID}
+      error={<p>Maps Load Error</p>}
+      loading={<p>Maps Loading...</p>}
+    >
+      <NaverMap
+        id="map"
+        mapDivId="react-naver-map"
+        style={{
+          width: "70%",
+          height: "400px",
+        }}
+        defaultCenter={{ lat: 37.49988, lng: 127.03856 }}
+        defaultZoom={16}
+        zoomControl={true} // 지도 zoom 허용
+      />
+    </RenderAfterNavermapsLoaded>
+  );
 };
 
 export default Map;

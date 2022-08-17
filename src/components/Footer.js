f9155c83f6a37f6c978dcd54804ec12c92be3bd3
김시환 <htogether7@gmail.com>
2022-08-18 01:56:17 +0900

    FEAT: Add map
    using naver maps, add map component

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div>제작자 : 김시환</div>;
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>피해피해</div>;
+};
+
+export default Header;
